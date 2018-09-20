19085f1e2c8bf9a372c40c0973e89bf79bf4e215
odow <o.dowson@gmail.com>
2018-09-20 10:53:25 -0500

    Introduce a check for the number of arguments and support complex numbers

--- src/nonlinear.jl
+++ src/nonlinear.jl
@@ -22,7 +22,7 @@
             "node_list": [
                 {
                     "head": "*", "args": [
-                        {"head": "constant", "value": 2},
+                        {"head": "real", "value": 2},
                         {"head": "variable", "name": "x"}
                     ]
                 },
@@ -36,7 +36,7 @@
                     "head": "^",
                     "args": [
                         {"head": "node", "index": 2},
-                        {"head": "constant", "value": 2}
+                        {"head": "real", "value": 2}
                     ]
                 },
                 {
@@ -53,40 +53,105 @@
 =#
 
 """
-    FUNCTION_TO_STRING
+    ARITY
 
-A dictionary that maps function names in Symbol form to their MathOptFormat
-string representation.
-
-If the list of functions that MathOptFormat supports is extended, a reverse
-entry should also be added to `STRING_TO_FUNCTION`.
+The arity of a nonlinear function. One of:
+ - `Nary` if the function accepts one or more arguments
+ - `Unary` if the function accepts exactly one argument
+ - `Binary` if the function accepts exactly two arguments.
 """
-const FUNCTION_TO_STRING = Dict{Symbol, String}(
-    :+ => "+",
-    :- => "-",
-    :* => "*",
-    :/ => "/",
-    :^ => "^",
-    :sin => "sin"
-)
+@enum ARITY begin
+    Nary
+    Unary
+    Binary
+end
+
+# A nice error message telling the user they supplied the wrong number of
+# arguments to a nonlinear function.
+function throw_invalid_arguments(function_name, function_type, num_arguments)
+    error("The function $(function_name) is an ($function_type) function, but" *
+          " you have passed $(num_arguments) arguments.")
+end
+
+function validate_arguments(function_name, actual::Int, required::ARITY)
+    if required == Nary && actual < 1
+        throw_invalid_arguments(function_name, "n-ary", actual)
+    elseif required == Unary && actual != 1
+        throw_invalid_arguments(function_name, "unary", actual)
+    elseif required == Binary && actual != 2
+        throw_invalid_arguments(function_name, "biary", actual)
+    end
+end
 
 """
-    STRING_TO_FUNCTION
+    SUPPORTED_FUNCTIONS
 
-A dictionary that maps function names in their MathOptFormat string
-representation to the symbol representing the Julia function.
-
-If the list of functions that MathOptFormat supports is extended, a reverse
-entry should also be added to `FUNCTION_TO_STRING`.
+A vector of string-symbol pairs that map the MathOptFormat string representation
+(i.e, the value of the `"head"` field) to the name of a Julia function (in
+Symbol form).
 """
-const STRING_TO_FUNCTION = Dict{String, Symbol}(
-    "+" => :+,
-    "-" => :-,
-    "*" => :*,
-    "/" => :/,
-    "^" => :^,
-    "sin" => :sin
-)
+const SUPPORTED_FUNCTIONS = Pair{String, Tuple{Symbol, ARITY}}[
+    # ==========================================================================
+    # The standard arithmetic functions.
+    # An n-ary function for addition: +(a, b, c) = a + b + c
+    "+" => (:+, Nary),
+    # An n-ary function for subtraction: -(a, b, c) = a - b - c
+    "-" => (:-, Nary),
+    # An n-ary function for multiplication: *(a, b, c) = a * b * c
+    "*" => (:*, Nary),
+    # A binary function for division. It must have exactly two arguments. The
+    # first argument is the numerator, the second argument is the denominator:
+    # /(a, b) = a / b.
+    "/" => (:/, Binary),
+    # ==========================================================================
+    # N-ary minimum and maximum functions.
+    "min" => (:min, Nary),
+    "max" => (:max, Nary),
+    # ==========================================================================
+    # The absolute value function: abs(x) = (x >= 0 ? x : -x).
+    "abs" => (:abs, Unary),
+    # ==========================================================================
+    # Log- and power-related functions.
+    # A binary function for exponentiation ^(a, b) = a ^ b.
+    "^" => (:^, Binary),
+    # The natural exponential function: exp(x) = e^x.
+    "exp" => (:exp, Unary),
+    # The base-e log function: y = log(x) => e^y = x.
+    "log" => (:log, Unary),
+    # The base-10 log function: y = log10(x) => 10^y = x.
+    "log10" => (:log10, Unary),
+    # The square root function: sqrt(x) = √x = x^(0.5).
+    "sqrt" => (:sqrt, Unary),
+    # ==========================================================================
+    # The unary trigonometric functions. These must have exactly one argument.
+    "cos" => (:cos, Unary),
+    "cosh" => (:cosh, Unary),
+    "acos" => (:acos, Unary),
+    "acosh" => (:acosh, Unary),
+    "sin" => (:sin, Unary),
+    "sinh" => (:sinh, Unary),
+    "asin" => (:asin, Unary),
+    "asinh" => (:asinh, Unary),
+    "tan" => (:tan, Unary),
+    "tanh" => (:tanh, Unary),
+    "atan" => (:atan, Unary),
+    "atanh" => (:atanh, Unary)
+]
+
+# An internal helper dictionary that maps function names in Symbol form to their
+# MathOptFormat string representation.
+const FUNCTION_TO_STRING = Dict{Symbol, Tuple{String, ARITY}}()
+
+# An internal helper dictionary that maps function names in their MathOptFormat
+# string representation to the symbol representing the Julia function.
+const STRING_TO_FUNCTION = Dict{String, Tuple{Symbol, ARITY}}()
+
+# Programatically add the list of supported functions to the helper dictionaries
+# for easy of look-up later.
+for (mathoptformat_string, (julia_symbol, num_arguments)) in SUPPORTED_FUNCTIONS
+    FUNCTION_TO_STRING[julia_symbol] = (mathoptformat_string, num_arguments)
+    STRING_TO_FUNCTION[mathoptformat_string] = (julia_symbol, num_arguments)
+end
 
 """
     convert_mof_to_expr(node::Object, node_list::Vector{Object})
@@ -96,8 +161,10 @@ MathOptFormat nodes in `node_list`.
 """
 function convert_mof_to_expr(node::Object, node_list::Vector{Object})
     head = node["head"]
-    if head == "constant"
+    if head == "real"
         return node["value"]
+    elseif head == "complex"
+        return Complex(node["real"], node["imag"])
     elseif head == "variable"
         return Symbol(node["name"])
     elseif head == "node"
@@ -106,8 +173,9 @@ function convert_mof_to_expr(node::Object, node_list::Vector{Object})
         if !haskey(STRING_TO_FUNCTION, head)
             error("Cannot convert MOF to Expr. Unknown function: $(head).")
         end
-        expr = Expr(:call)
-        push!(expr.args, STRING_TO_FUNCTION[head])
+        (julia_symbol, num_arguments) = STRING_TO_FUNCTION[head]
+        validate_arguments(head, length(node["args"]), num_arguments)
+        expr = Expr(:call, julia_symbol)
         for arg in node["args"]
             push!(expr.args, convert_mof_to_expr(arg, node_list))
         end
@@ -129,11 +197,13 @@ function convert_expr_to_mof(expr::Expr, node_list::Vector{Object})
     if !haskey(FUNCTION_TO_STRING, function_name)
         error("Cannot convert Expr to MOF. Unknown function: $(function_name).")
     end
+    (mathoptformat_string, num_arguments) = FUNCTION_TO_STRING[function_name]
+    validate_arguments(function_name, length(expr.args) - 1, num_arguments)
     node = Object(
-        "head" => FUNCTION_TO_STRING[function_name],
+        "head" => mathoptformat_string,
         "args" => Object[]
     )
-    for arg in expr.args[2:end]
+    for arg in @view(expr.args[2:end])
         push!(node["args"], convert_expr_to_mof(arg, node_list))
     end
     push!(node_list, node)
@@ -141,11 +211,17 @@ function convert_expr_to_mof(expr::Expr, node_list::Vector{Object})
 end
 
 # Recursion end for variables.
-function convert_expr_to_mof(expr::Symbol, node_list::Vector{Object})
-    return Object("head" => "variable", "name" => String(expr))
+function convert_expr_to_mof(sym::Symbol, node_list::Vector{Object})
+    return Object("head" => "variable", "name" => String(sym))
 end
 
-# Recursion end for numeric constants.
-function convert_expr_to_mof(expr::Real, node_list::Vector{Object})
-    return Object("head" => "constant", "value" => expr)
+# Recursion end for real constants.
+function convert_expr_to_mof(value::Real, node_list::Vector{Object})
+    return Object("head" => "real", "value" => value)
+end
+
+# Recursion end for complex numbers.
+function convert_expr_to_mof(value::Complex, node_list::Vector{Object})
+    return Object("head" => "complex", "real" => real(value),
+                  "imag" => imag(value))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -21,12 +21,34 @@ function roundtrip_nonlinear_expression(expr)
     @test MathOptFormat.convert_mof_to_expr(object, node_list) == expr
 end
 
-@testset "Roundtrip nonlinear expressions" begin
-    for expression in [2, 2.34, :x, :(1 + x), :(x - 1), :(x + y), :(x + y - z),
-                       :(2x), :(x * y), :(x / 2), :(2 / x), :(x / y),
-                       :(x / y / z), :(2^x), :(x^2), :(x^y), :(x^(2 * y + 1)),
-                       :(sin(x)), :(sin(x + y)), :(2x + sin(x)^2 + y)]
-        roundtrip_nonlinear_expression(expression)
+@testset "Nonlinear functions" begin
+    @testset "Error handling" begin
+        node_list = MathOptFormat.Object[]
+        # Test unsupported function for Expr -> MOF.
+        @test_throws Exception MathOptFormat.convert_expr_to_mof(
+            :(not_supported_function(x)), node_list)
+        # Test unsupported function for MOF -> Expr.
+        @test_throws Exception MathOptFormat.convert_mof_to_expr(
+            MathOptFormat.Object("head"=>"not_supported_function", "value"=>1),
+            node_list)
+        # Test n-ary function with no arguments.
+        @test_throws Exception MathOptFormat.convert_expr_to_mof(
+            :(min()), node_list)
+        # Test unary function with two arguments.
+        @test_throws Exception MathOptFormat.convert_expr_to_mof(
+            :(sin(x, y)), node_list)
+        # Test binary function with one arguments.
+        @test_throws Exception MathOptFormat.convert_expr_to_mof(
+            :(^(x)), node_list)
+    end
+    @testset "Roundtrip nonlinear expressions" begin
+        for expression in [2, 2.34, 2 + 3im, :x, :(1 + x), :(x - 1), :(x + y),
+                           :(x + y - z), :(2x), :(x * y), :(x / 2), :(2 / x),
+                           :(x / y), :(x / y / z), :(2^x), :(x^2), :(x^y),
+                           :(x^(2 * y + 1)), :(sin(x)), :(sin(x + y)),
+                           :(2x + sin(x)^2 + y), :(sin(3im)^2 + cos(3im)^2)]
+            roundtrip_nonlinear_expression(expression)
+        end
     end
 end
 
