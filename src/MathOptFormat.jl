b544942056302ce5e99b3f1e1bb595695bbd693f
Chris Coey <coey.chris@gmail.com>
2019-01-18 15:43:03 +1000

    use io instead of string for read/write functions

--- src/MathOptFormat.jl
+++ src/MathOptFormat.jl
@@ -8,4 +8,16 @@ include("MOF/MOF.jl")
 include("MPS/MPS.jl")
 include("CBF/CBF.jl")
 
+# TODO delete after https://github.com/JuliaOpt/MathOptInterface.jl/issues/633
+function MOI.read_from_file(model::MOI.ModelLike, filename::String)
+    open(filename, "r") do io
+        MOI.read_from_file(model, io)
+    end
+end
+function MOI.write_to_file(model::MOI.ModelLike, filename::String)
+    open(filename, "w") do io
+        MOI.write_to_file(model, io)
+    end
+end
+
 end
